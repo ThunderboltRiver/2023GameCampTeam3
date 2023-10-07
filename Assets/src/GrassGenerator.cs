8416c80420a99394ec770080f4a8374be76991b9
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-10-08 01:53:08 +0900

    GrassGeneratorの修正

--- Assets/src/GrassGenerator.cs
+++ Assets/src/GrassGenerator.cs
@@ -1,24 +1,23 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GrassGenerator : MonoBehaviour
 {
     [Header("Generate Setting")]
-    [SerializeField] private Vector2 size = new(5, 5);
-    [SerializeField] private int spawnCount;
-    [SerializeField] private int timeInterval = 20;
-    private int currentNum = 0;
+    [SerializeField] private Vector2 size;
+    [SerializeField] private int timeInterval;
+    [SerializeField] private int generateThreshold;
 
     [Header("Prefabs")]
-    [SerializeField] private GameObject[] grassPrefabs;
     [SerializeField] private GameObject[] enemyPrefabs;
-    [SerializeField] Grass[] grasses;
+    [SerializeField] GrassData[] grassDatas;
     //[SerializeField] Score score;
-
     [Header("Key Bindings")]
     [SerializeField] private KeyCode reGenerate = KeyCode.R;
 
     private float time;
+    private int generateCount;
 
     private void Update()
     {
@@ -29,30 +28,35 @@ public class GrassGenerator : MonoBehaviour
 
     private void FixedUpdate()
     {
-        time += Time.deltaTime;
-        if (Mathf.Round(time) % timeInterval == 0)
+        time += Time.deltaTime; // 時間を加算
+        var totalSeconds = Mathf.Round(time); // 経過時間のうち秒数だけ取得
+        if (totalSeconds % timeInterval == 0) // 一定時間ごとに作成
         {
-            SpawnStart();
+            GenerateGrass(increaseCoefficient: totalSeconds / timeInterval);
+            return;
         }
     }
 
-    private void SpawnStart()
+    private void GenerateGrass(float increaseCoefficient)
     {
-        foreach (Grass g in grasses)
+        var totalGrassWeight = grassDatas.Aggregate(0, (current, grassData) => current + grassData.Weight); // 出現率の合計を取得
+        foreach (var grassData in grassDatas)
         {
-            for (int i = 0; i < spawnCount * g.rate; i++)
+            var grassProbability = grassData.Weight / totalGrassWeight; // 出現率を確率に変換
+            var generateNum = grassProbability + increaseCoefficient; // 確率に応じて生成数を決定
+            for (var i = 0; i < generateNum; i++)
             {
-                Vector3 grassPos = new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.y / 2, size.y / 2));
-                SpawnGrass(grassPos);
+                var generatePosition = new Vector3(Random.Range(-size.x, size.x), 0, Random.Range(-size.y, size.y));
+                SpawnGrass(grassData.Prefab, generatePosition);
             }
-            if (currentNum < grasses.Length)
-                currentNum++;
         }
+
     }
 
-    private void SpawnGrass(Vector3 position)
+    private void SpawnGrass(GameObject grass,Vector3 position)
     {
-        GameObject grass = grassPrefabs[currentNum];
+        if(generateCount >= generateThreshold) return;
         Instantiate(grass, position, Quaternion.identity, transform);
+        generateCount++;
     }
 }
