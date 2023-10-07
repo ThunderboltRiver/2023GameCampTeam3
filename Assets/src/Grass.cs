7b04f0c872ed08cb7a12b19ceed32ded953cf24a
Wacky <SBA22G030002@edu.sba.ac.jp>
2023-10-07 16:59:32 +0900

    GrassGenerator仮完成

---
+++ Assets/src/Grass.cs
@@ -0,0 +1,5 @@
+[System.Serializable]
+public class Grass
+{
+    public int rate;
+}
--- Assets/src/GrassGenerator.cs
+++ Assets/src/GrassGenerator.cs
@@ -1,27 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GrassGenerator : MonoBehaviour
 {
+    [Header("Prefabs")]
     [SerializeField] private GameObject[] grassPrefabs;
     [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] Grass[] grasses;
 
+    [Header("Generate Setting")]
     [SerializeField] private Vector2 size = new(5, 5);
     [SerializeField] private int spawnCount;
+    private int currentNum = 0;
+
+    [Header("Key Bindings")]
+    [SerializeField] private KeyCode reGenerate = KeyCode.R;
 
     private void Start()
     {
-        for(int i = 0; i < spawnCount; i++)
+        SpawnStart();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(reGenerate))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void SpawnStart()
+    {
+        foreach (Grass g in grasses)
         {
-            Vector3 grassPos = new Vector3(Random.Range(0, size.x), 0, Random.Range(0, size.y));
-            SpawnGrass(grassPos);
+            for (int i = 0; i < spawnCount * g.rate; i++)
+            {
+                Vector3 grassPos = new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.y / 2, size.y / 2));
+                SpawnGrass(grassPos);
+            }
+            currentNum++;
         }
     }
 
     private void SpawnGrass(Vector3 position)
     {
-        GameObject enemy = grassPrefabs[Random.Range(0, grassPrefabs.Length)];
+        GameObject enemy = grassPrefabs[currentNum];
         Instantiate(enemy, position, Quaternion.identity, transform);
     }
 }
