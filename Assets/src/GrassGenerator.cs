aa60865636b666277e841b1f8f5823f48ef0ebe6
Wacky <SBA22G030002@edu.sba.ac.jp>
2023-10-07 14:52:08 +0900

    GrassGeneratorを更新

--- Assets/src/GrassGenerator.cs
+++ Assets/src/GrassGenerator.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GrassGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] grassPrefabs;
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [SerializeField] private Vector2 size = new(5, 5);
+    [SerializeField] private int spawnCount;
+
+    private void Start()
     {
-        
+        for(int i = 0; i < spawnCount; i++)
+        {
+            Vector3 grassPos = new Vector3(Random.Range(0, size.x), 0, Random.Range(0, size.y));
+            SpawnGrass(grassPos);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnGrass(Vector3 position)
     {
-        
+        GameObject enemy = grassPrefabs[Random.Range(0, grassPrefabs.Length)];
+        Instantiate(enemy, position, Quaternion.identity, transform);
     }
 }
